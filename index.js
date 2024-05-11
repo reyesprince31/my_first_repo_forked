d74cdc6f382ace9fb28628267eb28f4f5905e60c
Prince Reyes <77110899+reyesprince31@users.noreply.github.com>
2024-05-11 11:34:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { getRequest } from "./lib/getRequest.js";
 import { app } from "./app.js";
 
-const here = "hello";
+const here = "From: index.js, hello";
 console.log(here);
 console.log(app);
 console.log(getRequest);
